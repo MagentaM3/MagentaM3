aaee8dbb1c381431a788633e32a6bafd1722ab45
hyunseo-s <son020120@gmail.com>
2024-07-26 23:37:05 +1000

    login token is working, modified index.html to make it look like the default spotify auth webpage, but can be reverted later

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,64 +1,29 @@
-import { SpotifyApi } from "@spotify/web-api-ts-sdk";
 import * as trpcExpress from '@trpc/server/adapters/express';
+import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import express from 'express';
+import crypto from 'crypto';
+import express, { Request, Response } from 'express';
+import querystring from 'querystring';
+import request from 'request';
 import { renderTrpcPanel } from 'trpc-panel';
 import { seedDB } from './db/seed';
 import { env } from './env';
 import { LogModule, Logger } from './logging';
 import { appRouter } from './routers/_app';
 
-const clientId = env.SPOTIFY_CLIENT_ID;
-const clientSecret = env.SPOTIFY_CLIENT_SECRET;
-// const redirectUri = env.SPOTIFY_REDIRECT_URI;
+const clientId: string = env.SPOTIFY_CLIENT_ID;
+const clientSecret: string = env.SPOTIFY_CLIENT_SECRET;
+const redirectUri: string = env.SPOTIFY_REDIRECT_URI;
+const port: number = env.SERVER_PORT;
 
-// const searchSpotify = async () => {
-
-//   const api = SpotifyApi.withClientCredentials(
-//     clientId,
-//     clientSecret
-//   );
-
-//   try {
-//     const items = await api.search("The Beatles", ["artist"]);
-//     console.table(items.artists.items.map((item) => ({
-//       name: item.name,
-//       followers: item.followers.total,
-//       popularity: item.popularity,
-//     })));
-//   } catch (error) {
-//     console.error('Error searching Spotify:', error);
-//   }
-// };
-
-const searchSpotify = async () => {
-
-  console.log("Searching Spotify for The Beatles...");
-
-  const api = SpotifyApi.withClientCredentials(
-    clientId,
-    clientSecret
-  );
-
-  const items = await api.search("Frank", ["artist"]);
-
-  console.table(items.artists.items.map((item) => ({
-    name: item.name,
-    followers: item.followers.total,
-    popularity: item.popularity,
-  })));
-
+const generateRandomString = (length: number): string => {
+  return crypto
+    .randomBytes(60)
+    .toString('hex')
+    .slice(0, length);
 };
 
-
-const main = () => {
-  searchSpotify().catch(error => console.error(error));
-};
-
-main();
-
-
-const LM = new LogModule('INDEX');
+const stateKey: string = 'spotify_auth_state';
 
 const app = express();
 
@@ -69,12 +34,109 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use(express.static(__dirname + '/public'))
+  .use(cookieParser());
 
-// change below however you want, just want to test Spotify API
-app.get('/playlists', () => {
-  void (async () => {
-    // TODO implement a Spotify API call
-  })();
+app.get('/login', (req: Request, res: Response) => {
+  const state = generateRandomString(16);
+  res.cookie(stateKey, state);
+
+  // your application requests authorization
+  const scope = 'user-read-private user-read-email';
+  res.redirect('https://accounts.spotify.com/authorize?' +
+    querystring.stringify({
+      response_type: 'code',
+      client_id: clientId,
+      scope: scope,
+      redirect_uri: redirectUri,
+      state: state
+    }));
+});
+
+app.get('/callback', (req: Request, res: Response) => {
+  const code = req.query.code || null;
+  const state = req.query.state || null;
+  const storedState = req.cookies ? req.cookies[stateKey] : null;
+
+  console.log(`Received state: ${state}`);
+  console.log(`Stored state: ${storedState}`);
+
+  if (state === null || state !== storedState) {
+    res.redirect('/#' +
+      querystring.stringify({
+        error: 'state_mismatch'
+      }));
+  } else {
+    res.clearCookie(stateKey);
+    const authOptions = {
+      url: 'https://accounts.spotify.com/api/token',
+      form: {
+        code: code,
+        redirect_uri: redirectUri,
+        grant_type: 'authorization_code'
+      },
+      headers: {
+        'content-type': 'application/x-www-form-urlencoded',
+        Authorization: 'Basic ' + (Buffer.from(clientId + ':' + clientSecret).toString('base64'))
+      },
+      json: true
+    };
+
+    request.post(authOptions, (error: any, response: request.Response, body: any) => {
+      if (!error && response.statusCode === 200) {
+        const access_token = body.access_token,
+          refresh_token = body.refresh_token;
+
+        const options = {
+          url: 'https://api.spotify.com/v1/me',
+          headers: { Authorization: 'Bearer ' + access_token },
+          json: true
+        };
+
+        request.get(options, (error: any, response: request.Response, body: any) => {
+          console.log(body);
+        });
+
+        res.redirect('/#' +
+          querystring.stringify({
+            access_token: access_token,
+            refresh_token: refresh_token
+          }));
+      } else {
+        res.redirect('/#' +
+          querystring.stringify({
+            error: 'invalid_token'
+          }));
+      }
+    });
+  }
+});
+
+app.get('/refresh_token', (req: Request, res: Response) => {
+  const refresh_token = req.query.refresh_token;
+  const authOptions = {
+    url: 'https://accounts.spotify.com/api/token',
+    headers: {
+      'content-type': 'application/x-www-form-urlencoded',
+      Authorization: 'Basic ' + (Buffer.from(clientId + ':' + clientSecret).toString('base64'))
+    },
+    form: {
+      grant_type: 'refresh_token',
+      refresh_token: refresh_token
+    },
+    json: true
+  };
+
+  request.post(authOptions, (error: any, response: request.Response, body: any) => {
+    if (!error && response.statusCode === 200) {
+      const access_token = body.access_token,
+        refresh_token = body.refresh_token;
+      res.send({
+        access_token: access_token,
+        refresh_token: refresh_token
+      });
+    }
+  });
 });
 
 app.use(express.json({ limit: '50mb' }));
@@ -96,9 +158,7 @@ app.use('/panel', (_, res) => {
   return res.send(renderTrpcPanel(appRouter, { url: 'http://localhost:8080/trpc' }));
 });
 
-const port = env.SERVER_PORT;
-
 app.listen(port, async () => {
-  Logger.Info(LM, `Server is running on PORT=${port}`);
+  Logger.Info(new LogModule('INDEX'), `Server is running on PORT=${port}`);
   await seedDB();
 });
