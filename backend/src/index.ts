0ede76f2c4292b36d97a6db016531fa754703cea
felixcao <felixcao37@gmail.com>
2024-08-02 20:55:01 +1000

    provided stubs for syncing spotify data and added session implementation

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,39 +2,38 @@ import { AccessToken } from '@spotify/web-api-ts-sdk';
 import * as trpcExpress from '@trpc/server/adapters/express';
 import cookieParser from 'cookie-parser';
 import cors from 'cors';
-import crypto from 'crypto';
 import express, { Request, Response } from 'express';
-import querystring from 'querystring';
+import session from 'express-session';
 import request from 'request';
 import { renderTrpcPanel } from 'trpc-panel';
 import { seedDB } from './db/seed';
 import { env } from './env';
 import { LogModule, Logger } from './logging';
 import { appRouter } from './routers/_app';
-import {
-  getCurrentUserPlaylists,
-  getCurrentUserProfile,
-  getPlaylistItems
-} from './spotifyAPI';
+import { createContext } from './trpc';
 
 const clientId: string = env.SPOTIFY_CLIENT_ID;
 const clientSecret: string = env.SPOTIFY_CLIENT_SECRET;
 const redirectUri: string = env.SPOTIFY_REDIRECT_URI;
 const port: number = env.SERVER_PORT;
 
-const generateRandomString = (length: number): string => {
-  return crypto
-    .randomBytes(60)
-    .toString('hex')
-    .slice(0, length);
-};
-
-const stateKey: string = 'spotify_auth_state';
+declare module 'express-session' {
+  interface SessionData {
+    accessToken: AccessToken;
+  }
+}
 
 const app = express();
 
+app.use(session({
+  secret: 'your-secret-key',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { secure: false } // Set to true if using HTTPS
+}));
+
 const corsOptions = {
-  origin: '*',
+  origin: 'http://localhost:5173',
   credentials: true,
   optionSuccessStatus: 200,
 };
@@ -43,27 +42,9 @@ app.use(cors(corsOptions));
 app.use(express.static(__dirname + '/public'))
   .use(cookieParser());
 
-app.get('/login', (req: Request, res: Response) => {
-  const state = generateRandomString(16);
-  res.cookie(stateKey, state);
-
-  // your application requests authorization
-  const scope = 'user-read-private user-read-email';
-  res.redirect('https://accounts.spotify.com/authorize?' +
-    querystring.stringify({
-      response_type: 'code',
-      client_id: clientId,
-      scope: scope,
-      redirect_uri: redirectUri,
-      state: state
-    }));
-});
-
 app.get('/callback', (req: Request, res: Response) => {
-  console.log("callback called")
   const code = req.query.code || null;
 
-  res.clearCookie(stateKey);
   const authOptions = {
     url: 'https://accounts.spotify.com/api/token',
     form: {
@@ -78,53 +59,11 @@ app.get('/callback', (req: Request, res: Response) => {
     json: true
   };
 
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   request.post(authOptions, async (error: any, response: request.Response, body: any) => {
-
     if (!error && response.statusCode === 200) {
-      const accessToken: AccessToken = {
-        access_token: body.access_token,
-        token_type: body.token_type,
-        expires_in: body.expires_in,
-        refresh_token: body.refresh_token,
-        // expires?:
-      }
-
-      const curUser = await getCurrentUserProfile(clientId, accessToken);
-      const playlists = await getCurrentUserPlaylists(clientId, accessToken);
-      const playlistItems = await getPlaylistItems(clientId, accessToken, "6TQ5jTvSWkZ3fgcjmZ3zcK");
-
-      console.log(curUser);
-    }
-  });
-  res.json({
-    hello: "yo"
-  })
-});
-
-
-app.get('/refresh_token', (req: Request, res: Response) => {
-  const refresh_token = req.query.refresh_token;
-  const authOptions = {
-    url: 'https://accounts.spotify.com/api/token',
-    headers: {
-      'content-type': 'application/x-www-form-urlencoded',
-      Authorization: 'Basic ' + (Buffer.from(clientId + ':' + clientSecret).toString('base64'))
-    },
-    form: {
-      grant_type: 'refresh_token',
-      refresh_token: refresh_token
-    },
-    json: true
-  };
-
-  request.post(authOptions, (error: any, response: request.Response, body: any) => {
-    if (!error && response.statusCode === 200) {
-      const access_token = body.access_token,
-        refresh_token = body.refresh_token;
-      res.send({
-        access_token: access_token,
-        refresh_token: refresh_token
-      });
+      req.session.accessToken = body;
+			res.redirect('http://localhost:5173/collection');
     }
   });
 });
@@ -141,6 +80,7 @@ app.use(
   '/trpc',
   trpcExpress.createExpressMiddleware({
     router: appRouter,
+		createContext
   }),
 );
 
---
+++ backend/src/services/spotify.ts
@@ -0,0 +1,40 @@
+import { AccessToken } from '@spotify/web-api-ts-sdk';
+import { getSpotifyApi } from '../spotifyApi';
+
+export const syncSpotifyData = async (accessToken: AccessToken) => {
+	await syncUserProfile(accessToken);
+	await syncUserPlaylists(accessToken);
+}
+
+export const syncUserProfile = async (accessToken: AccessToken) => {
+	const spotifyApi = getSpotifyApi(accessToken);
+	const profile = await spotifyApi.currentUser.profile();
+	console.log(profile);
+}
+
+export const syncUserPlaylists = async (accessToken: AccessToken) => {
+	const spotifyApi = getSpotifyApi(accessToken);
+	// TODO: handle > 50 playlists
+	const playlists = await spotifyApi.currentUser.playlists.playlists();
+	console.log(playlists);
+	// TODO: add filter for playlists with up-to-date snapshot id
+	await Promise.all(playlists.items.map(p => syncPlaylist(accessToken, p.id)));
+}
+
+export const syncPlaylist = async (accessToken: AccessToken, playlistId: string) => {
+	const spotifyApi = getSpotifyApi(accessToken);
+
+	const playlist = await spotifyApi.playlists.getPlaylist(playlistId);
+
+	const playlistItemsRequests = [];
+
+	const limit = 50;
+	for (let offset = 0; offset < playlist.tracks.total; offset += limit) {
+		playlistItemsRequests.push(spotifyApi.playlists.getPlaylistItems(playlistId, undefined, '', limit, offset));
+	}
+
+	const playlistItems = await Promise.all(playlistItemsRequests);
+
+	console.log(playlist);
+	console.log(playlistItems);
+}
