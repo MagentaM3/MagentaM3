1c235effc10e397f604190d18b6ca78c3c4a2e59
felixcao <felixcao37@gmail.com>
2024-08-04 21:14:15 +1000

    removed spotifyApi file and moved contents to spotify.ts

--- backend/src/services/spotify.ts
+++ backend/src/services/spotify.ts
@@ -1,5 +1,10 @@
-import { AccessToken } from '@spotify/web-api-ts-sdk';
-import { getSpotifyApi } from '../spotifyApi';
+import { AccessToken, SpotifyApi } from '@spotify/web-api-ts-sdk';
+import { env } from '../env';
+
+const clientId: string = env.SPOTIFY_CLIENT_ID;
+
+export const getSpotifyApi = (accessToken: AccessToken): SpotifyApi =>
+	SpotifyApi.withAccessToken(clientId, accessToken);
 
 export const syncSpotifyData = async (accessToken: AccessToken) => {
 	await syncUserProfile(accessToken);
--- backend/src/spotifyAPI.ts
+++
@@ -1,7 +0,0 @@
-import { AccessToken, SpotifyApi } from '@spotify/web-api-ts-sdk';
-import { env } from './env';
-
-const clientId: string = env.SPOTIFY_CLIENT_ID;
-
-export const getSpotifyApi = (accessToken: AccessToken): SpotifyApi =>
-	SpotifyApi.withAccessToken(clientId, accessToken);
