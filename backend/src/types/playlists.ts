29c2ac89adfd8ce71ec8327f66b333a1d266970d
hyunseo-s <son020120@gmail.com>
2024-08-04 21:04:25 +1000

    moved both playlist implementations to playlists.ts

--- backend/src/types/playlists.ts
+++ backend/src/types/playlists.ts
@@ -1,13 +1,19 @@
-// getPlaylist is for getting the tracks and putting them as rows in the table.
-// Info about each track's album is important but I don't think we need to go
-// to the extent of knowing the album's artists (we alr have the song's artists)
-
 import { z } from 'zod';
 import { Image } from './image.ts';
 import { PlaylistTrack } from './playlistTrack.ts';
 import { User } from './user.ts';
 
-export const PlaylistComplex = z.object({
+export const SimplifiedPlaylist = z.object({
+    id: z.number(),
+    // collaborative: z.boolean(),
+    description: z.string(),
+    images: z.array(Image),
+    name: z.string(),
+    owner: User,
+    uri: z.string()
+});
+
+export const Playlist = z.object({
     id: z.number(),
     // collaborative: z.boolean(),
     description: z.string(),
@@ -19,4 +25,5 @@ export const PlaylistComplex = z.object({
     uri: z.string()
 });
 
-export type PlaylistComplexZ = z.input<typeof PlaylistComplex>;
+export type SimplifiedPlaylistZ = z.input<typeof SimplifiedPlaylist>;
+export type NotSimplifiedPlaylistZ = z.input<typeof Playlist>;
