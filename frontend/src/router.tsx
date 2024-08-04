2c511a809f7e632b55cb754aac036dd4c936ab36
felixcao <felixcao37@gmail.com>
2024-08-05 00:03:33 +1000

    setting up frontend

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -1,13 +1,13 @@
 import { createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 
-import { CollectionPage } from "./pages/CollectionPage";
-import { LandingPage } from "./pages/LandingPage";
+import LandingPage from "@/pages/LandingPage";
+import PlaylistPage from "@/pages/PlaylistPage";
 
 export const router = createBrowserRouter(
   createRoutesFromElements(
     <>
       <Route path="/" element={<LandingPage />} />
-			<Route path="/collection" element={<CollectionPage />} />
+			<Route path="/playlist/:playlistId" element={<PlaylistPage />} />
     </>
   )
 );
