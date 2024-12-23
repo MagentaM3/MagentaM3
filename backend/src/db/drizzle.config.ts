b7c98d872f790252fc7e888fcda22b025a4e2594
felixcao <felixcao37@gmail.com>
2024-12-22 22:56:09 -0500

    updated backend for drizzle studio

--- backend/src/db/drizzle.config.ts
+++ backend/src/db/drizzle.config.ts
@@ -11,9 +11,10 @@ export default defineConfig({
   dbCredentials: {
     user: env.DB_USER,
     password: env.DB_PASSWORD,
-    host: env.DB_HOST,
+    host: 'localhost',
     port: env.DB_PORT,
     database: env.DB_NAME,
+		ssl: false
   },
   strict: true,
 });
